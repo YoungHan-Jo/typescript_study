863523850ac43499bdd6bc6ddf9abd9f6836c0d1
YoungHan Jo <verin4494@gmail.com>
2023-05-17 15:33:22 +0900

    study interface

--- src/study/interface.ts
+++ src/study/interface.ts
@@ -55,7 +55,7 @@ if (myBeer.hope && myBeer.hope < 10) {
 
 
 // read only property
-enum Gender{
+enum Gender {
     Male,
     Female
 }
@@ -66,7 +66,7 @@ interface User {
     readonly birthday: number;
 }
 
-let user: User ={
+let user: User = {
     name: "Jack",
     age: 20,
     gender: Gender.Male,
@@ -79,16 +79,16 @@ console.log(user);
 
 
 // interface　合わせ
-interface FullName{
+interface FullName {
     firstName: string;
     lastName: string;
 }
 
-interface FullName{
+interface FullName {
     middleName: string;
 }
 
-const fullName: FullName ={
+const fullName: FullName = {
     firstName: "A",
     lastName: "C",
     middleName: "B"
@@ -110,23 +110,148 @@ let dev: Developer = {
     skill: "JAVA"
 }
 
-interface Address{
+interface Address {
     address: string;
 }
 
-interface User extends Developer,Address {}
+interface IUser extends Developer, Address { }
 
-let userA: User = {
-    name: "",
-    age: 0,
-    gender: Gender.Male,
-    birthday: 0,
+let iUser: IUser = {
     skill: "",
+    name: "",
     address: ""
 }
 
 
 // call signature
+interface Login {
+    (username: string, password: string): boolean;
+}
+
+let loginUser: Login = (id, pw) => {
+    console.log(id, 'login success')
+    return true;
+}
+
+console.log(loginUser('Sam', 'password'));
+
+
+interface GetText {
+    (name: string, age: number): string;
+    totalCall?: number;
+}
+
+const getText2: GetText = (name, age) => {
+    if (getText2.totalCall !== undefined) {
+        getText2.totalCall += 1;
+        console.log(`totalCall: ${getText2.totalCall}`);
+    }
+    return '';
+}
+
+getText2.totalCall = 0;
+getText2('sam', 23)
+getText2('jam', 24)
+getText2('ram', 25)
+
+interface Object {
+    getShortKeys(this: object): string[];
+}
+
+Object.prototype.getShortKeys = function () {
+    return Object.keys(this).filter((key) => key.length <= 3);
+}
+
+const obj = {
+    a: 11,
+    bb: 22,
+    ccc: 33,
+    dddd: 44
+}
+
+console.log(obj.getShortKeys());
+
+
+// interface function overload
+interface Add {
+    (x: number, y: number): number;
+    (x: string, y: string): string;
+}
+const iAdd: Add = (x: any, y:any) => x + y;
+
+console.log(iAdd('hello','world'))
+console.log(iAdd(1,2))
+
+
+interface UserInterface {
+    name: string;
+    getName(): string;
+}
+
+class UserClass implements UserInterface {
+    name: string;
+
+	constructor(name: string) {
+        this.name = name;
+	}
+    
+    getName(): string {
+        return this.name;
+    }
+}
+
+const sam = new UserClass("sam")
+console.log(sam.getName());
+
+// construct signature
+interface CatConstrutor{
+    new (name: string): Cat;
+}
+
+class Cat{
+
+	constructor(public name: string) {
+	}
+}
+
+function makeKitten(c: CatConstrutor, n: string){
+    return new c(n);
+}
+
+const kitten = makeKitten(Cat,'Lucy');
+console.log(kitten.name);
+
+
+// indexable type
+
+type Score = 'A' | 'B' | 'C' | 'D' | 'F'
+
+interface UserIF {
+    name: string;
+    [grade: number]: Score;
+}
+
+const userA: UserIF = {
+    name: "James",
+    1: 'A'
+}
+const userB: UserIF = {
+    name: "Cindy",
+    3: 'B'
+}
+
+
+interface Item {
+    [itemIndex: number]: string
+}
+
+let item: Item = ['a','b','c']
+
+console.log(item[0]);
+console.log(item[1]);
+
+
+
 
 
 
